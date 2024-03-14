a306bc79e594d11b248af4afe278f4a2e8ad6a06
leona <leonardovieiraxy@hotmail.com>
2024-03-14 18:46:47 -0300

    Changes to be committed: 	renamed:    Unidade03/03 - singlePageAplication/01 - angular-js-hello-world/index.html -> Unidade03/03 - singlePageAplication/01-angular-js-hello-world/01-index.html 	new file:   Unidade03/03 - singlePageAplication/01-angular-js-hello-world/02-index.html 	renamed:    Unidade03/03 - singlePageAplication/01 - angular-js-hello-world/js/app.js -> Unidade03/03 - singlePageAplication/01-angular-js-hello-world/js/app.js 	renamed:    Unidade03/03 - singlePageAplication/01 - angular-js-hello-world/lib/angular.js -> Unidade03/03 - singlePageAplication/01-angular-js-hello-world/lib/angular.js 	new file:   Unidade03/03 - singlePageAplication/01-angular-js-hello-world/lib/angular.min.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/habito.php 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/index.html 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/js/app.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/lib/angular-route.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/lib/angular-route.min.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/lib/angular.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/lib/angular.min.js 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/partials/api-test-delete.html 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/partials/api-test-get.html 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/partials/api-test-post.html 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/partials/api-test-put.html 	new file:   Unidade03/03 - singlePageAplication/listadehabitos-api-test/partials/welcome.html

---
+++ Unidade03/03 - singlePageAplication/listadehabitos-api-test/js/app.js
@@ -0,0 +1,304 @@
+// // Declaramos o módulo, que é ligado à
+// // página Web através da diretiva ng-app
+// // ngRoute na linha abaixo é uma injeção de
+// // dependência, é um módulo que está contido
+// // do arquivo angular-route.js
+// var app = angular.module("app", ["ngRoute"]);
+
+// // Declaramos o controller para a página
+// // api-test-get.html
+// app.controller("apitestgetcontroller", [
+//   "$scope",
+//   "$http",
+//   function ($scope, $http) {
+//     $scope.result = "";
+//     $scope.id = "";
+//     $scope.listaDeHabitosApiGet = function () {
+//       if ($scope.id == "") {
+//         $http
+//           .get(
+//             "http://localhost:80/unidade03/listadehabitos-api-test/habito.php"
+//           )
+//           .then(function (response) {
+//             var output = JSON.stringify(response.data);
+//             $scope.result = output;
+//           });
+//       } else {
+//         $http
+//           .get(
+//             "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+//             {
+//               params: {
+//                 id: $scope.id,
+//               },
+//             }
+//           )
+//           .then(function (response) {
+//             var output = JSON.stringify(response.data);
+//             $scope.result = output;
+//           });
+//       }
+//     };
+//   },
+// ]);
+
+// // Declaramos o controller para a página
+// // api-test-post.html
+// app.controller("apitestpostcontroller", [
+//   "$scope",
+//   "$http",
+//   function ($scope, $http) {
+//     $scope.result = "";
+//     $scope.nome = "";
+//     // Criar
+//     $scope.listaDeHabitosApiPost = function () {
+//       $http
+//         .post(
+//           "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+//           {
+//             nome: $scope.nome,
+//           }
+//         )
+//         .then(function (response) {
+//           var output = JSON.stringify(response.data);
+//           $scope.result = output;
+//         });
+//     };
+//   },
+// ]);
+
+// // Declaramos o controller para a página
+// // api-test-put.html
+// app.controller("apitestputcontroller", [
+//   "$scope",
+//   "$http",
+//   function ($scope, $http) {
+//     $scope.result = "";
+//     $scope.id = "";
+//     $scope.nome = "";
+//     $scope.status = "";
+//     // Atualizar
+//     $scope.listaDeHabitosApiPut = function () {
+//       $http
+//         .put(
+//           "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+//           {
+//             id: $scope.id,
+//             nome: $scope.nome,
+//             status: $scope.status,
+//           }
+//         )
+//         .then(function (response) {
+//           var output = JSON.stringify(response.data);
+//           $scope.result = output;
+//         });
+//     };
+//   },
+// ]);
+
+// // Declaramos o controller para a página
+// // api-test-get.html
+// app.controller("apitestdeletecontroller", [
+//   "$scope",
+//   "$http",
+//   function ($scope, $http) {
+//     $scope.result = "";
+//     $scope.id = "";
+//     // Deletar
+//     $scope.listaDeHabitosApiDel = function () {
+//       $http
+//         .delete(
+//           "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+//           {
+//             params: {
+//               id: $scope.id,
+//             },
+//           }
+//         )
+//         .then(function (response) {
+//           var output = JSON.stringify(response.data);
+//           $scope.result = output;
+//         });
+//     };
+//   },
+// ]);
+
+// // Declaramos o controller para a página
+// // welcome.html
+// app.controller("welcomecontroller", ["$scope", function ($scope) {}]);
+
+// // Aqui definimos a dinâmica de navegação da
+// // página. Para cada URL que a página possui
+// // define-se qual controller será usado
+// // e qual arquivo .html será exibido
+// app.config(function ($routeProvider) {
+//   $routeProvider
+//     .when("/api-test-get", {
+//       controller: "apitestgetcontroller",
+//       templateUrl: "partials/api-test-get.html",
+//     })
+//     .when("/api-test-post", {
+//       controller: "apitestpostcontroller",
+//       templateUrl: "partials/api-test-post.html",
+//     })
+//     .when("/api-test-put", {
+//       controller: "apitestputcontroller",
+//       templateUrl: "partials/api-test-put.html",
+//     })
+//     .when("/api-test-delete", {
+//       controller: "apitestdeletecontroller",
+//       templateUrl: "partials/api-test-delete.html",
+//     })
+//     .otherwise({
+//       controller: "welcomecontroller",
+//       templateUrl: "partials/welcome.html",
+//     });
+// });
+
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/api-test-get", {
+      controller: "apitestgetcontroller",
+      templateUrl: "partials/api-test-get.html",
+    })
+    .when("/api-test-post", {
+      controller: "apitestpostcontroller",
+      templateUrl: "partials/api-test-post.html",
+    })
+    .when("/api-test-put", {
+      controller: "apitestputcontroller",
+      templateUrl: "partials/api-test-put.html",
+    })
+    .when("/api-test-delete", {
+      controller: "apitestdeletecontroller",
+      templateUrl: "partials/api-test-delete.html",
+    })
+    .otherwise({
+      controller: "welcomecontroller",
+      templateUrl: "partials/welcome.html",
+    });
+});
+
+app.controller("welcomecontroller", function ($scope) {
+  $scope.message = "Bem-vindo ao meu aplicativo!";
+});
+
+// Declaramos o controller para a página
+// api-test-get.html
+app.controller("apitestgetcontroller", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.result = "";
+    $scope.id = "";
+    $scope.listaDeHabitosApiGet = function () {
+      if ($scope.id == "") {
+        $http
+          .get(
+            "http://localhost:80/unidade03/listadehabitos-api-test/habito.php"
+          )
+          .then(function (response) {
+            var output = JSON.stringify(response.data);
+            $scope.result = output;
+          });
+      } else {
+        $http
+          .get(
+            "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+            {
+              params: {
+                id: $scope.id,
+              },
+            }
+          )
+          .then(function (response) {
+            var output = JSON.stringify(response.data);
+            $scope.result = output;
+          });
+      }
+    };
+  },
+]);
+
+// Declaramos o controller para a página
+// api-test-post.html
+app.controller("apitestpostcontroller", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.result = "";
+    $scope.nome = "";
+    // Criar
+    $scope.listaDeHabitosApiPost = function () {
+      $http
+        .post(
+          "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+          {
+            nome: $scope.nome,
+          }
+        )
+        .then(function (response) {
+          var output = JSON.stringify(response.data);
+          $scope.result = output;
+        });
+    };
+  },
+]);
+
+// Declaramos o controller para a página
+// api-test-put.html
+app.controller("apitestputcontroller", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.result = "";
+    $scope.id = "";
+    $scope.nome = "";
+    $scope.status = "";
+    // Atualizar
+    $scope.listaDeHabitosApiPut = function () {
+      $http
+        .put(
+          "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+          {
+            id: $scope.id,
+            nome: $scope.nome,
+            status: $scope.status,
+          }
+        )
+        .then(function (response) {
+          var output = JSON.stringify(response.data);
+          $scope.result = output;
+        });
+    };
+  },
+]);
+
+// Declaramos o controller para a página
+// api-test-get.html
+app.controller("apitestdeletecontroller", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.result = "";
+    $scope.id = "";
+    // Deletar
+    $scope.listaDeHabitosApiDel = function () {
+      $http
+        .delete(
+          "http://localhost:80/unidade03/listadehabitos-api-test/habito.php",
+          {
+            params: {
+              id: $scope.id,
+            },
+          }
+        )
+        .then(function (response) {
+          var output = JSON.stringify(response.data);
+          $scope.result = output;
+        });
+    };
+  },
+]);
